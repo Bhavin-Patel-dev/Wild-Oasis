85f08d3f6428d496c91a375959117d75a8a2f11d
Bhavin Patel <bhavin.2304patel@gmail.com>
2026-05-20 16:45:34 +0530

    protected backend key (sensitive part)

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://scivfcmmuwygtzoahwqc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjaXZmY21tdXd5Z3R6b2Fod3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4Mzc0NDQsImV4cCI6MjA3MTQxMzQ0NH0.A9QWY6ve2DenNYu3VbggGBpImI5Pu_WkIZPmRTN4T6M";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
